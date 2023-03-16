9f847b7ad123c704eeaa9ff85337b91afbe18168
perladaghfal <perla.daghfal@hotmail.com>
2023-03-16 18:16:59 +0200

    search bar

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
+import './Home.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
-class Home extends React.Component{
-    render() {
-        return (
-            <div>
-                <h1>Home</h1>
-            </div>
-        );
-    }
+class Home extends React.Component {
+  render() {
+    return (
+      <div className="wrap">
+        <div className="search">
+          <input type="text" className="searchTerm" placeholder="What are you looking for?" />
+          <button type="submit" className="searchButton">
+            <FontAwesomeIcon icon={faSearch} />
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Home;
