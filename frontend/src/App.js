9c58b3e174ec7c5189514a68da279fb4775224ac
Ryan Atallah <ryan@R-MacBook-Pro.local>
2023-03-16 17:08:40 +0200

    Fixing Home

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,18 @@
 import './App.css';
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import Header from './components/Header/Header';
-
+import Home from './pages/Home/Home';
 
 function App() {
   return (
     <div className="App">
-
+      <Header />
+      <Home />
     </div>
   );
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
+root.render(<App />);
 export default App;
