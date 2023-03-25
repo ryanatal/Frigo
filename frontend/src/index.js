8e6d89e12a869fb76575d5cb70c043cbf09a4e1a
omar issa <oissa@omars-MacBook-Pro.local>
2023-03-25 12:36:50 +0200

    added bootstrap CDN

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom';
+
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
