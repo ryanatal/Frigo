971d561d4a3c2811968a50a66881c7c0bd3818b4
perladaghfal <perla.daghfal@hotmail.com>
2023-03-28 00:34:16 +0300

    update recipe scroll

--- frontend/src/pages/Recipe/Recipe.jsx
+++ frontend/src/pages/Recipe/Recipe.jsx
@@ -1,90 +1,40 @@
 import React from 'react';
-import './Recipe.css';
-import recipe_img from '../../assets/recipe1.jpg';
+import { useState, useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
+import { getRecipeInfo} from "../../services/ApiService";
 
+export const Recipe = () => {
+  const [recipe, setRecipe] = useState({});
+  const  id  = 1;
 
-class Recipe extends React.Component {
-  render() {
-    return (
+  useEffect(() => {
+      getRecipeInfo(id).then((recipe) => setRecipe(recipe));
+    }, [id]);
+
+  return (
     <div>
       <div id="searchbar">
         <input id="search_input" type="text" placeholder="Search for recipes" />
-        <input type = "button" id = "shopping_list" value = "Shopping List"/>
+        <input type="button" id="shopping_list" value="Shopping List" />
       </div>
-      
- 
+
       <div id="slider">
-      <div id="container">
-
-      <div class="card">
-         <img id="recipe_img" src={recipe_img} alt="recipe_img" />
-            <div class="content">
-                <h2 class="title">Truffle Pasta</h2>
-                <p class="description">Truffle cheese, heavy cream, baby bella mushrooms, garlic, and pasta.</p>
-                <div class="button">View Recipe</div>
-            </div>
-
-      </div>
-
-      <div class="card">
-         <img id="recipe_img" src={recipe_img} alt="recipe_img" />
-            <div class="content">
-                <h2 class="title">Recipe Title</h2>
-                <p class="description">Whole wheat pizza, pesto sauce, burrata cheese, and cherry.</p>
-                <div class="button">View Recipe</div>
-            </div>
-
-      </div>
-      <div class="card">
-         <img id="recipe_img" src={recipe_img} alt="recipe_img" />
-            <div class="content">
-                <h2 class="title">Recipe Title</h2>
-                <p class="description">Short description of the recipe</p>
-                <div class="button">View Recipe</div>
-            </div>
-
-      </div>
-      <div class="card">
-         <img id="recipe_img" src={recipe_img} alt="recipe_img" />
-            <div class="content">
-                <h2 class="title">Recipe Title</h2>
-                <p class="description">Short description of the recipe</p>
-                <div class="button">View Recipe</div>
-            </div>
-
-      </div>
-      <div class="card">
-         <img id="recipe_img" src={recipe_img} alt="recipe_img" />
-            <div class="content">
-                <h2 class="title">Recipe Title</h2>
-                <p class="description">Short description of the recipe</p>
-                <div class="button">View Recipe</div>
-            </div>
-
-      </div>
-      <div class="card">
-         <img id="recipe_img" src={recipe_img} alt="recipe_img" />
-            <div class="content">
-                <h2 class="title">Recipe Title</h2>
-                <p class="description">Short description of the recipe</p>
-                <div class="button">View Recipe</div>
-            </div>
-
-      </div>
-      <div class="card">
-         <img id="recipe_img" src={recipe_img} alt="recipe_img" />
-            <div class="content">
-                <h2 class="title">Recipe Title</h2>
-                <p class="description">Short description of the recipe</p>
-                <div class="button">View Recipe</div>
-            </div>
-
-      </div>
+        <div id="container">
+          <div className="card">
+          <div className="header">
+              <img src={recipe.image} alt={recipe.title} className="cardImage"></img>
+          </div>
+          <div className="text">
+              <h1 className="food">
+                  {recipe.title}
+              </h1>
+          </div>
+              <Link to={`/recipe/${id}`} className="btn">view more</Link>
+          </div>
+        </div>
       </div>
     </div>
-    </div>
-    );
-  }
-}
+  );
+};
 
 export default Recipe;
