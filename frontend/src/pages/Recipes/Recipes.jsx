64a56590a29ebead842e61b7d37720c81dde28a7
perladaghfal <perla.daghfal@hotmail.com>
2023-03-29 01:06:19 +0300

    recipes update

--- frontend/src/pages/Recipes/Recipes.jsx
+++ frontend/src/pages/Recipes/Recipes.jsx
@@ -5,6 +5,7 @@ import { getRecipeInfo} from "../../services/ApiService";
 import "./Recipes.scss";
 import "../CoverRecipe/CoverRecipe.scss";
 import RecipeCard from "../../components/RecipeCard/RecipeCard";
+
 export const Recipes = () => {
   const [recipe, setRecipe] = useState({});
   const  id  = 1;
@@ -14,18 +15,17 @@ export const Recipes = () => {
     }, [id]);
 
   return (
-    <div id="perla">
-      <div id="searchbar">
-        <input id="search_input" type="text" placeholder="Search for recipes" />
-        <input type="button" id="shopping_list" value="Shopping List" />
-      </div>
-
-      <div id="sliderPerla">
-        <div id="containerPerla">
-          <RecipeCard id = "1"/>
-          <RecipeCard id = "1"/>
-          <RecipeCard id = "1"/>
+    <div id="main">
+       <div id="searchContainer">
+          <input id="searchInput" type="text" placeholder="Search for recipes" />
         </div>
+
+      <div id="slider">
+            <RecipeCard id="1"/>
+            <RecipeCard id="1"/>
+            <RecipeCard id="1"/>
+            <RecipeCard id="1"/>
+            <RecipeCard id="1"/>
       </div>
     </div>
   );
