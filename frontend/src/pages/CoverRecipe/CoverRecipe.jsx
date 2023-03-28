2097a5625348461f4f7cdfabbc91bef60718c96a
Ryan Atallah <ryanatallah02@gmail.com>
2023-03-28 10:08:42 +0300

    Creating Recipe Cards

--- frontend/src/pages/CoverRecipe/CoverRecipe.jsx
+++ frontend/src/pages/CoverRecipe/CoverRecipe.jsx
@@ -1,41 +1,14 @@
 import React from 'react';
 import './CoverRecipe.scss';
-import { useState, useEffect } from "react";
-import { Link, useLocation } from "react-router-dom";
-import {
-    getRecipeInfo
-} from "../../services/ApiService";
-
+import RecipeCard from "../../components/RecipeCard/RecipeCard";
 
 export const CoverRecipe = () => {
-    const [recipe, setRecipe] = useState({});
-    //gets id from url
-    const  id  = 1;
-
-    useEffect(() => {
-        getRecipeInfo(id).then((recipe) => setRecipe(recipe));
-      }, [id]);
-
-
     return (
         <>
-        <div className="CoverRecipe">
-            <div className="Intro">
-                <h1>What's for dinner?</h1>
-            </div>
-            <div className="card">
-                <div className="header">
-                    <img src={recipe.image} alt={recipe.title} className="cardImage"></img>
-                </div>
-                <div className="text">
-                    <h1 className="food">
-                        {recipe.title}
-                    </h1>
-                    <p className="info">ETA: {recipe.readyInMinutes} &nbsp; &nbsp; Likes: {recipe.aggregateLikes}</p>
-                </div>
-                    <Link to={`/recipes/${id}`} className="btn">Let's Cook!</Link>
-            </div>
+        <div className="Intro">
+            <h1>What's for dinner?</h1>
         </div>
+        <RecipeCard id="1" />
         </>
     );
 };
--- frontend/src/pages/Recipes/Recipes.jsx
+++ frontend/src/pages/Recipes/Recipes.jsx
@@ -3,7 +3,8 @@ import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { getRecipeInfo} from "../../services/ApiService";
 import "./Recipes.scss";
-
+import "../CoverRecipe/CoverRecipe.scss";
+import RecipeCard from "../../components/RecipeCard/RecipeCard";
 export const Recipes = () => {
   const [recipe, setRecipe] = useState({});
   const  id  = 1;
@@ -13,51 +14,18 @@ export const Recipes = () => {
     }, [id]);
 
   return (
-    <div>
+    <div id="perla">
       <div id="searchbar">
         <input id="search_input" type="text" placeholder="Search for recipes" />
         <input type="button" id="shopping_list" value="Shopping List" />
       </div>
 
-      <div id="slider">
-        <div id="container">
-          <div className="card">
-          <div className="header">
-              <img src={recipe.image} alt={recipe.title} className="cardImage"></img>
-          </div>
-          <div className="text">
-              <h1 className="food">
-                  {recipe.title}
-              </h1>
-          </div>
-              <Link to={`/recipes/${id}`} className="btn">view more</Link>
-          </div>
-        
-        <div className="card">
-          <div className="header">
-              <img src={recipe.image} alt={recipe.title} className="cardImage"></img>
-          </div>
-          <div className="text">
-              <h1 className="food">
-                  {recipe.title}
-              </h1>
-          </div>
-              <Link to={`/recipes/${id}`} className="btn">view more</Link>
-          </div>
-        
-        <div className="card">
-          <div className="header">
-              <img src={recipe.image} alt={recipe.title} className="cardImage"></img>
-          </div>
-          <div className="text">
-              <h1 className="food">
-                  {recipe.title}
-              </h1>
-          </div>
-              <Link to={`/recipes/${id}`} className="btn">view more</Link>
-          </div>
+      <div id="sliderPerla">
+        <div id="containerPerla">
+          <RecipeCard id = "1"/>
+          <RecipeCard id = "1"/>
+          <RecipeCard id = "1"/>
         </div>
-
       </div>
     </div>
   );
