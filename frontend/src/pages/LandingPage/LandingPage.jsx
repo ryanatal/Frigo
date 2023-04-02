55755cd53a7caaec832cd2b0e56d43f8534cee0e
Ryan Atallah <104354805+ryanatal@users.noreply.github.com>
2023-04-02 23:32:21 +0300

    Scroll feature OnClick Arrow

--- frontend/src/pages/LandingPage/LandingPage.jsx
+++ frontend/src/pages/LandingPage/LandingPage.jsx
@@ -1,17 +1,21 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './LandingPage.scss';
 import {CoverRecipe} from '../CoverRecipe/CoverRecipe';
 import CoverPage from '../CoverPage/CoverPage';
 
-class LandingPage extends React.Component {
-    render() {
-      return (
-        <div className="Landing">
-            <CoverPage />
-            <CoverRecipe />
-        </div>
-      );
-    }
-  }
+const LandingPage = () => {
   
-  export default LandingPage;
+  const coverRecipeRef = useRef(null);
+
+  return (
+    <div className="Landing">
+      <CoverPage coverRecipeRef={coverRecipeRef} />
+      <div ref={coverRecipeRef}>
+        <CoverRecipe />
+      </div>
+    </div>
+  );
+}
+
+  
+export default LandingPage;
