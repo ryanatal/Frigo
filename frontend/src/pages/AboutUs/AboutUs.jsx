8746b011cecd8fc69a17ff9c297e4e8bdcc11953
Ryan Atallah <104354805+ryanatal@users.noreply.github.com>
2023-04-10 01:46:25 +0300

    New Images for About us

--- frontend/src/pages/AboutUs/AboutUs.jsx
+++ frontend/src/pages/AboutUs/AboutUs.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import "./AboutUs.scss";
-
+import RyanA from "../../assets/About-Us/Ryan-Atallah-About.png";
+import PerlaD from "../../assets/About-Us/Perla-Daghfal-About.png";
+import OmarI from "../../assets/About-Us/Omar-Issa-About.png";
+import RyanK from "../../assets/About-Us/Ryan-Khalil-About.png";
 const AboutUs = () => {
   return (
     <div className="aboutus">
@@ -8,22 +11,22 @@ const AboutUs = () => {
        <h2>Our Team</h2>
       <div className="team-container">
         <div className="team-member">
-          <img src="https://via.placeholder.com/150" alt="Team member 1" />
+          <img src = {RyanA} alt="Team member 1" />
           <h3>Ryan Atallah</h3>
           <p>Full-stack developer</p>
         </div>
         <div className="team-member">
-          <img src="https://via.placeholder.com/150" alt="Team member 2" />
+          <img src = {PerlaD} alt="Team member 2" />
           <h3>Perla Daghfal</h3>
           <p>Full-stack developer</p>
         </div>
         <div className="team-member">
-          <img src="https://via.placeholder.com/150" alt="Team member 3" />
+          <img src = {OmarI} alt="Team member 3" />
           <h3>Omar Issa</h3>
           <p>Full-stack developer</p>
         </div>
         <div className="team-member">
-          <img src="https://via.placeholder.com/150" alt="Team member 4" />
+          <img src = {RyanK} alt="Team member 4" />
           <h3>Ryan Khalil</h3>
           <p>Full-stack developer</p>
         </div>
