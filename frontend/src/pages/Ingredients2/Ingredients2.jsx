7ee117531183fea7a21800c6b4cd54efa924733b
Ryan Khalil <rfk27@mail.aub.edu>
2023-04-13 21:08:54 +0300

    searchbar now functional

--- frontend/src/pages/Ingredients2/Ingredients2.jsx
+++ frontend/src/pages/Ingredients2/Ingredients2.jsx
@@ -5,6 +5,7 @@ import "./Ingredients2.scss";
 
 export const Ingredients2 = () => {
     const [ingredientData, setIngredientData] = useState([]);
+    const [searchQuery, setSearchQuery] = useState('');
 
     useEffect(() => {
         async function fetchIngredientData() {
@@ -12,27 +13,47 @@ export const Ingredients2 = () => {
             setIngredientData(data);
         }
         fetchIngredientData();
-        }, []);
+    }, []);
 
+    const handleSearchChange = (event) => {
+        setSearchQuery(event.target.value);
+    };
+
+    const filteredIngredientData = ingredientData.filter(
+        (ingredient) =>
+            ingredient.ingredient.toLowerCase().includes(searchQuery.toLowerCase())
+    );
 
     return (
         <>
             <div id="searchContainerIngredients">
-                <input id="searchInputIngredients" type="text" placeholder="Search for ingredients" />
-                <input type = "button" id = "getIngredients" value = "View Ingredients"/>
+                <input
+                    id="searchInputIngredients"
+                    type="text"
+                    placeholder="Search for ingredients"
+                    value={searchQuery}
+                    onChange={handleSearchChange}
+                />
+                <input type="button" id="getIngredients" value="View Ingredients" />
             </div>
             <div id="sliderIngredient">
                 <div className="card-deck">
                     <div className="ingredientCard card">
-                        <div class="ingredientContent card-body">
-                            <h4 class="card-title ingredientTitle">Ingredients</h4>
-                            <div class="card-text ingredientList">
-                                {ingredientData.map((ingredient) => (
+                        <div className="ingredientContent card-body">
+                            <h4 className="card-title ingredientTitle">Ingredients</h4>
+                            <div className="card-text ingredientList">
+                                {filteredIngredientData.map((ingredient) => (
                                     <div className="ingredient-details">
-                                        <input type="checkbox" class="ingredientCheckbox" id={ingredient.id} name={ingredient.ingredient} value={ingredient.ingredient}></input>
+                                        <input
+                                            type="checkbox"
+                                            className="ingredientCheckbox"
+                                            id={ingredient.id}
+                                            name={ingredient.ingredient}
+                                            value={ingredient.ingredient}
+                                        />
                                         &nbsp;
                                         &nbsp;
-                                        <label for={ingredient.ingredient}>{ingredient.ingredient}</label>
+                                        <label htmlFor={ingredient.ingredient}>{ingredient.ingredient}</label>
                                     </div>
                                 ))}
                             </div>
