c017aa85680e66f3d1591529fa9bfdad393bd448
Ryan Atallah <104354805+ryanatal@users.noreply.github.com>
2023-04-19 15:19:40 +0300

    Removing backend from this repository

--- backend/routes/pantry.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { addIngredients, getIngredients } from '../controllers/pantry.js';
-
-const router = express.Router();
-
-router.post('/add', addIngredients);
-router.get('/get', getIngredients);
-
-export default router;
--- backend/routes/shoppinglist.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { addItems, getItems } from '../controllers/shoppinglist.js';
-
-const router = express.Router();
-
-router.post('/add', addItems);
-router.get('/get', getItems);
-
-export default router;
