84004a2922581ad1ae195aba146731eff01bdaed
omar issa <oissa@omars-MacBook-Pro.local>
2023-04-19 20:02:37 +0300

    testing route shopping

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/aboutus" element={<AboutUs />} />
-          <Route path="/shopping" element={<ShoppingList />} />
+          <Route path="/Frigo/shopping" element={<ShoppingList />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/login" element={<Login />} />
           <Route path="/recipes/:id" element={<RecipeDetails />} />
