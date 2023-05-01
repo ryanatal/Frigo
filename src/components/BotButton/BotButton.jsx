20e27a8d66a6876db79721f97a6e0ca45814333c
Ryan Atallah <104354805+ryanatal@users.noreply.github.com>
2023-05-01 13:00:53 +0300

    adding a Modal for chatbot

--- src/components/BotButton/BotButton.jsx
+++ src/components/BotButton/BotButton.jsx
@@ -1,29 +1,25 @@
 import React, { useState } from 'react';
 import Bot from '../Bot/Bot';
 import Button from 'react-bootstrap/Button';
-import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
-import Popover from 'react-bootstrap/Popover';
+import Modal from 'react-bootstrap/Modal';
 import "./BotButton.scss";
 
 function BotButton() {
     const [showBot, setShowBot] = useState(false);
     
-    const popover = (
-        <Popover id="popover-basic">
-            <Popover.Header as="h3">FrigoBot</Popover.Header>
-            <Popover.Body>
-                {showBot && <Bot />}
-            </Popover.Body>
-        </Popover>
-    );
-
     return (
         <>
-            <OverlayTrigger trigger="click" placement="left" overlay={popover}>
-                <Button variant="success" onClick={() => setShowBot(prevShowBot => !prevShowBot)} style={{position: 'fixed', bottom: 0, right: 0}}>
-                    Show Bot
-                </Button>
-            </OverlayTrigger>
+            <Button variant="success" onClick={() => setShowBot(true)} style={{position: 'fixed', bottom: 0, right: 0}}>
+                Show Bot
+            </Button>
+            <Modal show={showBot} onHide={() => setShowBot(false)}>
+                <Modal.Header closeButton>
+                    <Modal.Title>FrigoBot</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Bot />
+                </Modal.Body>
+            </Modal>
         </>
     );
 }
