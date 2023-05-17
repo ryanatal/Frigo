360c2e6c6fb731cb91b7fb28914a95424a6cbf1f
Ryan Atallah <104354805+ryanatal@users.noreply.github.com>
2023-05-17 13:07:01 +0300

    Adding Linked in links to about us

--- src/pages/AboutUs/AboutUs.jsx
+++ src/pages/AboutUs/AboutUs.jsx
@@ -11,30 +11,40 @@ const AboutUs = () => {
     <div className="about-us-container">
        <h2>Our Team</h2>
       <div className="team-container">
-        <div className="team-member">
-          <img src = {RyanA} alt="Team member 1" />
+      <div className="team-member">
+        <a href="https://www.linkedin.com/in/ryan-atallah2002/" target="_blank" rel="noreferrer">
+          <img src={RyanA} alt="Team member 1" />
           <h3>Ryan Atallah</h3>
           <p>Full-stack developer</p>
+        </a>
+      </div>
+        <div className="team-member">
+          <a href="https://www.linkedin.com/in/perla-daghfal-171638249/" target="_blank" rel="noreferrer">
+            <img src = {PerlaD} alt="Team member 2" />
+            <h3>Perla Daghfal</h3>
+            <p>Full-stack developer</p>
+          </a>
         </div>
         <div className="team-member">
-          <img src = {PerlaD} alt="Team member 2" />
-          <h3>Perla Daghfal</h3>
-          <p>Full-stack developer</p>
+          <a href="https://www.linkedin.com/in/omar-issa24/" target="_blank" rel="noreferrer">
+            <img src = {OmarI} alt="Team member 3" />
+            <h3>Omar Issa</h3>
+            <p>Full-stack developer</p>
+          </a>
         </div>
         <div className="team-member">
-          <img src = {OmarI} alt="Team member 3" />
-          <h3>Omar Issa</h3>
-          <p>Full-stack developer</p>
+          <a href="https://www.linkedin.com/in/ryankhalil/" target="_blank" rel="noreferrer">
+            <img src = {RyanK} alt="Team member 4" />
+            <h3>Ryan Khalil</h3>
+            <p>Full-stack developer</p>
+          </a>
         </div>
         <div className="team-member">
-          <img src = {RyanK} alt="Team member 4" />
-          <h3>Ryan Khalil</h3>
-          <p>Full-stack developer</p>
-        </div>
-        <div className="team-member">
-          <img src = {DrGeorges} alt="Professor" />
-          <h3>Dr. Georges Hatem</h3>
-          <p>Supervising Professor</p>
+          <a href="https://www.linkedin.com/in/georgeshatem/" target="_blank" rel="noreferrer">
+            <img src = {DrGeorges} alt="Professor" />
+            <h3>Dr. Georges Hatem</h3>
+            <p>Supervising Professor</p>
+          </a>
         </div>
       </div>
     </div>
