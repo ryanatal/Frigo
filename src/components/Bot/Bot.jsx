ca0f5f1926978a117b99d9ec7fa8151cc33ea80b
Omar Issa <oissa2424@gmail.com>
2023-05-18 21:55:52 +0300

    added key

--- src/components/Bot/Bot.jsx
+++ src/components/Bot/Bot.jsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
 import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from '@chatscope/chat-ui-kit-react';
 
-const API_KEY = "";
+const API_KEY = "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";
 
 const systemMessage = { //  Change this here to whatever you want frigobot to talk like. Explain like xxx
   "role": "system", "content": "Explain things like you're a Chef teaching students"
